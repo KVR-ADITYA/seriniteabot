f15182a75c132d7dccf9b5f7af6bea4848e3332b
KVR-ADITYA <34878205+KVR-ADITYA@users.noreply.github.com>
2021-05-27 19:37:35 +0530

    emoji?

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
 client = discord.Client()
 
+emoji = discord.utils.get(message.guild.emojis, name="angy_tubby")
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -16,7 +18,7 @@ async def on_message(message):
     if message.author ==  client.user:
         return
     if message.content.startswith('!hello'):
-        await message.channel.send('Kono aida :angy_tubby:')
+        await message.channel.send(f'Kono aida {emoji}')
         await channel.send(file=discord.File('./images/angly_tubby.png'))
 
 
